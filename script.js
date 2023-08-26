d229e48e8c38c5ffc34e9259d474d4ab13efd74a
humasbpkdki <humasbpkdki@gmail.com>
2023-08-26 18:49:43 +0700

    update

--- script.js
+++ script.js
@@ -41,6 +41,10 @@ document.addEventListener("DOMContentLoaded", function() {
          } else {
             modal.show();
          }
+
+         // Mengatur tampilan gambar kunci.png pada button yang diklik
+         const kunciImg = button.querySelector(".kunci-img");
+         kunciImg.classList.add("active");
       });
    });
 
@@ -58,6 +62,16 @@ document.addEventListener("DOMContentLoaded", function() {
       } else if (passwordInputValue === correctPassword) {
          sessionStorage.setItem(targetUrl, "true");
          modal.hide();
+
+         // Menghapus tampilan gambar kunci.png pada button yang diklik
+         const buttons = document.querySelectorAll(".toggleModal");
+         buttons.forEach(button => {
+            const kunciImg = button.querySelector(".kunci-img");
+            if (button.getAttribute("data-url") === targetUrl) {
+               kunciImg.classList.remove("active");
+            }
+         });
+         
          window.location.href = targetUrl;
       } else {
          alert.textContent = "Maaf Password yang anda masukan salah, silahkan hubungi Subbag Humas!";
